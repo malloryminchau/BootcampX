9beca78e8bf8edea64bff7251bdfe7ce59ce70a2
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-09-16 22:06:36 +0000

    2 queries files for joinging tables

---
+++ 2_queries_joins/cohorts_total_assignment_duration.sql
@@ -0,0 +1,6 @@
+SELECT SUM(duration) as total_duration
+FROM assignment_submissions 
+JOIN students ON assignment_submissions.student_id = students.id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = 'FEB12';
+
